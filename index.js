8c6c9bdcef6c86c64847bfcda7562141d6840d28
Mark <mgnorrish@gmail.com>
2023-03-06 17:04:26 +1100

    implement basic /new

--- index.js
+++ index.js
@@ -3,13 +3,9 @@ dotenv.config();
 // const cors = require('cors')({origin: true});
 const express = require('express');
 const app = express();
-const nodemailer = require("nodemailer");
-const path = require("path");
 
 const firebaseConfig = require('./firebase-config.json');
-const { initializeApp } = require('firebase-admin/app');
-const { signInWithEmailAndPassword, getAuth } = require("firebase/auth");
-const { doc, getFirestore, setDoc } = require("firebase/firestore");
+const { addDoc, doc, getDoc, runTransaction, serverTimestamp, setDoc } = require("firebase/firestore");
 
 
 const admin = require('firebase-admin');
@@ -21,30 +17,62 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-      user: process.env.EMAIL,
-      pass: process.env.EMAIL_PW
-  }
-});
-transporter.sendMail({from:"server",to:process.env.EMAIL,subject:"test email", html:"<p>test</p>"});
-// const c = cors(req, res, ()=> {
-//   transporter.sendMail({from:"server",to:process.env.EMAIL,subject:"test email", html:"<p>test</p>"}, (err,info) => {
-//     console.log(err);
-//     console.log(info);
-//   })
-// });
-// console.log(c);
-// console.log(typeof(c));
+/*
+DB should have:
+* name
+* time
+* location
+* description
+* ?endTime
+* ?maxGuests
+* !{boys, girls}{registered, invited}
+
+Registrants should have:
+* name
+* email
+* sex
+* ?deadline
+*/
+
+function sanityCheck(body, res, fields) {
+  fields.forEach(field => {
+    if (!body[field]) {
+      res.sendStatus(400);
+      res.send(String(field) + " missing");
+      return true;
+    }
+  });
+  return false;
+}
+
+const sexes = ["male", "female"]; // aspirational: move to stored in event, so that users can rename as they like
 
 // Handler for POST request to /events
-app.post('/events', async (req, res) => {
+// TODO include functionality for event creator to insta-register
+app.post('/new', async (req, res) => {
+  const body = req.body;
+  if (sanityCheck(body, res, ["name", "time", "location", "description"])) {
+    return;
+  }
+  if (new Date() > new Date(body.time)) {
+    res.sendStatus(400);
+    res.send("time must be in future");
+    return;
+  }
   try {
-    await setDoc(doc(db, "events", 23), {"one": 1, "two":2});
-    // const eventData = req.body; // Assuming data is sent as JSON in the request body
-    // const db = admin.firestore();
-    // await db.collection('events').add(eventData); // Add the data to the 'events' collection in Firestore
+    const e = {
+      "name": body.name, 
+      "time": body.time, 
+      "location": body.location, 
+      "description": body.description, 
+      "endTime": new Date(new Date(body.time).getTime()+60000)
+    };
+    sexes.forEach(sex => {
+      ["Registered", "Invited"].forEach(stage => {
+        e[sex+stage] = {};
+      })
+    });
+    await addDoc(doc(db, "events"), e);
     res.sendStatus(201); // Send a success status code
   } catch (error) {
     console.error(error);
@@ -52,24 +80,49 @@ app.post('/events', async (req, res) => {
   }
 });
 
+
 app.post("/register", async (req, res) => {
+  const body = req.body;
+  if (sanityCheck(body, res, ["name", "email", "sex", "eventId"])) {
+    return;
+  }
+
   try {
-    const name = req.name;
-    const email = req.email;
-    const sex = req.sex;
-    if (name && email && sex) {
-      await setDoc(doc(db, "events", req.params.eventId), {name:name});
-      res.sendStatus(201)
-    } else {
-      res.sendStatus(400);
-      res.send("<div>Name, email, and sex must all be supplied.</div>")
-    }
-    await setDoc(doc(db, "events", ), )
+    await runTransaction(db, async (t) => {
+      const event = await getDoc(doc(db, "events", body.eventId));
+      const oppositeSex = body.sex == "male" ? "female" : "male"; // TODO this is ugly
+      const potentialPartners = event[oppositeSex+"Registered"];
+      var isInvited = false;
+      const expiredRegistrants = []
+      while (Object.keys(potentialPartners).length) {
+        // then take the first registree from there to Invited, put this one onto Invited, and send emails;
+        // or rather make this into a while, removing any whose deadline has expired
+        const partner = potentialPartners; // TODO .get whichever has the lowest timestamp;
+        if (new Date(partner.timestamp) < new Date()) {
+          // remove that partner, both here and in the DB
+          expiredRegistrants.push(partner);
+          delete potentialPartners[partner]; // does this work?
+        } else {
+          // invite both
+          isInvited = true;
+          potentialPartners = []; // so that we exist the loop
+          // send emails; move both to the Invited sets
+        }
+      } 
+
+      if (!isInvited) {
+        // set the doc's sex Registered to also include the registrant, with this timestamp
+        // TODO this should absolutely be factored out, so that /new and /register reuse code
+        return {
+          timestamp: serverTimestamp()
+        }
+      }
+    })
   } catch (err) {
     console.error(err);
     res.sendStatus(500);
   }
-})
+});
 
 app.get('/hello', async (req, res) => {
   res.send('Hello, World! Adding page because why not!');
