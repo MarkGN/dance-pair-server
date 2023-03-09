b57b26349eba738881bbed1aeefdd6e184788301
Mark <mgnorrish@gmail.com>
2023-03-09 23:30:23 +1100

    create and retrieve event

--- functions/index.js
+++ functions/index.js
@@ -1,39 +1,21 @@
+require("dotenv").config();
 const functions = require("firebase-functions");
 const cors = require('cors')({origin: true});
 // const cors = require('cors')({origin: ['https://example.com', 'https://www.example.com']});
 // ... to make it only accept from, say, my front-end
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = functions.https.onRequest((req, res) => {
-//   cors(req, res, () => {
-//     functions.logger.info("Hello logs!", {structuredData: true});
-//     res.send("Hello from Firebase!");
-//   });
-// });
-
-exports.helloWorld = functions.https.onRequest((req, res) => {
-  console.log(req.headers)
-  cors(req, res, () => {
-    res.status(200).send('Hello, World!');
-  });
-});
-
-const dotenv = require("dotenv");
-dotenv.config();
-
-const firebaseConfig = require("./firebase-config.json");
-const {addDoc, doc, getDoc, runTransaction, serverTimestamp, setDoc } = require("@firebase/firestore");
-
+const { initializeApp } = require("@firebase/app");
+const {addDoc, collection, doc, getDoc, getFirestore, runTransaction, serverTimestamp, setDoc } = require("@firebase/firestore");
 const admin = require("firebase-admin");
+const firebaseConfig = require("./firebase-config.json");
 
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 admin.initializeApp({
   credential: admin.credential.cert(firebaseConfig),
   databaseURL: process.env.DB_URL,
 });
 
-const db = admin.firestore();
+
 /*
 DB should have:
 * name
@@ -51,10 +33,11 @@ Registrants should have:
 * ?deadline
 */
 
-function sanityCheck(body, fields) {
+function sanityCheck(res, body, fields) {
   fields.forEach((field) => {
     if (!body[field]) {
-      return field;
+      res.status(500).send({error: String(field) + " missing"});
+      return true;
     }
   });
   return false;
@@ -64,35 +47,78 @@ const sexes = ["male", "female"];
 // TODO: store in event, so that users can rename as they like
 
 exports.new = functions.https.onRequest(async (req, res) => {
-  const body = req.body;
-  const check = sanityCheck(body, ["name", "time", "location", "description"]);
-  if (check) {
-    res.sendStatus(500);
-    res.send(check + " missing");
-  }
-  if (new Date() > new Date(body.time)) {
-    res.sendStatus(500);
-    res.send("time must be in future");
-    return;
-  }
-  try {
-    const e = {
-      "name": body.name,
-      "time": body.time,
-      "location": body.location,
-      "description": body.description,
-      "endTime": new Date(new Date(body.time).getTime()+60000),
-    };
-    sexes.forEach((sex) => {
-      ["Registered", "Invited"].forEach((stage) => {
-        e[sex+stage] = {};
+  cors(req, res, async () => {
+    const body = req.body;
+    if (sanityCheck(res, body, ["name", "time", "location", "description"])) {
+      return;
+    }
+    if (new Date() > new Date(body.time)) {
+      res.status(500).send("time must be in future");
+      return;
+    }
+    try {
+      const endTime = body.time; //body.endTime || new Date(new Date(body.time).getTime()+3600000);
+      const e = {
+        "name": body.name,
+        "time": body.time,
+        "location": body.location,
+        "description": body.description
+      };
+      sexes.forEach((sex) => {
+        ["Registered", "Invited"].forEach((stage) => {
+          e[sex+stage] = {};
+        });
       });
-    });
-    const eventId = await addDoc(doc(db, "events"), e);
-    res.sendStatus(201);
-    res.send(eventId);
-  } catch (error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
+      if (body.user) {
+        const user = body.user;
+        const userCheck = sanityCheck(user, ["name", "email", "sex"]);
+        if (userCheck) {
+          res.status(500).send("malformed user data, aborting");
+        }
+        e[user.sex + "Registered"] = {
+          "name" : user.name,
+          "email" : user.email,
+          "sex" : user.sex,
+          "deadline" : user.deadline || endTime,
+          "timestamp" : serverTimestamp(),
+        }
+      }
+      const eventId = await addDoc(collection(db, "events"), e);
+      res.status(201).send(eventId);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error submitting event" + error);
+    }
+  });
+});
+
+exports.event = functions.https.onRequest(async (req, res) => {
+  cors(req, res, async () => {
+    const body = req.body; // was req.body
+    if (sanityCheck(res, body, ["id"])) {
+      return;
+    }
+    try {
+      const event = (await getDoc(doc(db, "events", body.id))).data();
+      // TODO delete other unnecessary data
+      sexes.forEach((sex) => {
+        ["Registered","Invited"].forEach((state) => {
+          event[sex+state] = Object.keys(event[sex+state]).length;
+        });
+      });
+      // TODO redact this sensitive info
+      if (event) {
+        res.status(200).send(event);
+      } else {
+        res.status(404).send("event not found");
+      }      
+    } catch (error) {
+      console.error(error);
+      // res.status(500).send({error: "error fetching event"});
+      if (!res.headersSent) {
+        res.status(500).send({error: "error fetching event"});
+      }
+      return; // I don't see how this will help
+    }
+  });
 });
